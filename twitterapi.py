542a69acdfad74a8cd8861ae6b52f9e855f502cb
Cesar <cesdia883@gmail.com>
2020-09-03 21:29:54 -0400

    write csv of postids, issue when reading file

--- twitterapi.py
+++ twitterapi.py
@@ -1,5 +1,7 @@
 import requests
 from requests_oauthlib import OAuth1Session
+import csv
+import os
 
 
 class API:
@@ -18,12 +20,14 @@ class API:
                              resource_owner_secret=self.access_token_secret,
                              signature_type='auth_header')
         return oath
-    
-    def stringify(self,id):
-        if type(id) =='str':
+
+
+    def stringify(self, id):
+        if type(id) == 'str':
             return id
         else:
             return str(id)
+
 
     def user_id(self):
         protected_url = 'https://api.twitter.com/1.1/account/verify_credentials.json'
@@ -31,14 +35,113 @@ class API:
         r = oauth.get(protected_url)
         r_json = r.json()
         return r_json['id']
-    
-    def post_date(self,id):
+
+
+    def initial_timeline_request(self):
+        protected_url = 'https://api.twitter.com/1.1/statuses/user_timeline.json'
+        oauth = self.authenticate()
+        user_id = self.user_id
+        parameters = {
+            'user_id': self.stringify(user_id),
+            'count': 200,
+            'include_rts': 1
+        }
+        r = oauth.get(protected_url, params=parameters)
+        return r.json()
+
+
+    def continual_timeline_request(self, max_id):
+        """        
+        :reference: https://developer.twitter.com/en/docs/twitter-api/v1/tweets/timelines/api-reference/get-statuses-user_timeline
+        """
+
+        protected_url = 'https://api.twitter.com/1.1/statuses/user_timeline.json'
+        oauth = self.authenticate()
+        user_id = self.user_id
+        parameters = {
+            'user_id': self.stringify(user_id),
+            'count': 200,
+            'include_rts': 1,
+            'max_id': max_id
+        }
+        r = oauth.get(protected_url, params=parameters)
+        return r.json()
+
+
+    def all_timeline_posts(self):
+        """        
+        :reference: https://developer.twitter.com/en/docs/twitter-api/v1/tweets/timelines/api-reference/get-statuses-user_timeline
+        """
+
+        all_posts = []
+        posts = self.initial_timeline_request()
+        all_posts.extend(posts)  # save the initial request to list
+        max_id = all_posts[-1].get('id') - 1  # save the id of the oldest tweet minus 1
+
+        while len(posts) > 0:  # while still requesting, status 200 request length is 200
+            print(f'Getting posts before {max_id}')
+            posts = self.continual_timeline_request(max_id)
+            all_posts.extend(posts)
+            max_id = all_posts[-1].get('id') - 1
+            print(f'{len(all_posts)} posts downloaded so far')
+
+        cleaned_all_posts = [[post.get('id'), post.get('created_at')] for post in all_posts]
+
+        with open('Account_PostsID.csv',
+                  'w') as file:  # write the IDs to a CSV
+            writer = csv.writer(file)
+            writer.writerow(["PostID","Created At"])
+            writer.writerows(post for post in cleaned_all_posts)
+
+        pass
+
+
+    def post_data(self, post_id):
+        """
+        "reference: https://developer.twitter.com/en/docs/twitter-api/tweets/lookup/api-reference/get-tweets-id
+        """
         str_id = self.stringify(id)
         protected_url = f'https://api.twitter.com/2/tweets/{str_id}'
         oauth = self.authenticate()
         parameters = {
-            'expansions': 'entities.mentions.username',
-            'tweet.fields': 'created_at,entities,non_public_metrics,public_metrics,text'
+            'expansions':
+            'entities.mentions.username',
+            'tweet.fields':
+            'created_at,entities,non_public_metrics,organic_metrics,public_metrics,text'
         }
         r = oauth.get(protected_url, params=parameters)
         return r.json()
+
+
+    def all_post_ids(self):
+        if os.path.exists('Account_PostsID.csv'):  # if files exists
+            with open('Account_PostsID.csv', 'r') as file:
+                reader = csv.reader(file)
+                all_post_ids = [row for row in reader]
+                print(f'Your IDs file has {len(all_post_ids)} rows')
+                return all_post_ids
+
+        self.all_timeline_posts()  # else run script and write file
+        self.all_post_ids()  # run again to check condition
+        pass
+
+
+    def test_post_data(self):
+        post_ids = self.all_post_ids()
+        all_post_data = []
+        for post_id in post_ids[:3]:    #TODO: 'NoneType' object is not subscriptable
+            post_data = self.post_data(post_id[0])  # self.allpost_ids returns a list of id and created_at
+            all_post_data.extend(post_data)
+
+        print(all_post_data)
+        pass
+
+
+    def all_post_data(self):
+        post_ids = self.all_post_ids
+        all_post_data = []
+        for post_id in post_ids:
+            post_data = self.post_data(post_id[0])  #  self.allpost_ids returns a list of id and created_at
+            all_post_data.extend(post_data)
+        
+        pass
