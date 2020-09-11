97a8754a3bcbf10de99ab59c740404a263d67d0f
Cesar <cesdia883@gmail.com>
2020-09-11 14:48:03 -0400

    adding notes to methods

--- data.py
+++ data.py
@@ -1,20 +1,22 @@
 import pandas as pd
-import os
-
-from twitterapi import API
 
 class CleanPostFrame:
     def __init__(self, dataframe):
-        self.dataframe = data_frame
-        self.new_df = None
+        self.dataframe = dataframe
 
     def post_info(self, index):
+        """
+        returns pd.Series type object containing a df rows info
+        """
         df_series = self.dataframe.iloc[index]
         return df_series
 
     def series_to_dict(self):
+        """
+        Converts all the rows to pd.Series to Dicts
+        """
         all_dicts = []
-        for i in range(2): # change to len(self.dataframe)
+        for i in range(len(self.dataframe)):
             post_info = self.post_info(i)
             series_to_dict = pd.Series.to_dict(post_info)
             # print(series_to_dict)
@@ -22,6 +24,10 @@ class CleanPostFrame:
         return all_dicts
     
     def inner_dict_keys(self, i):
+        """
+        json data contained nested dicts of two levels
+        grabbing the keys for the innermost dict for a single row
+        """
         df_series = self.post_info(i)
         series_values = df_series.values
 
@@ -37,6 +43,9 @@ class CleanPostFrame:
         return flat_dict_keys
 
     def inner_keys(self):
+        """
+        returning the inner keys for the entire dataframe
+        """
         all_keys = []
         for i in range(len(self.dataframe)):
             all_keys.append(self.inner_dict_keys(i))
@@ -48,10 +57,16 @@ class CleanPostFrame:
         return inner_keys
 
     def flat_headers(self):
+        """
+        creating df headers using the unique inner keys and the non-dict outer keys
+        """
         inner_keys = self.inner_keys()
         return inner_keys + ['created_at', 'id', 'text']
 
     def update_dict(self, _dict):
+        """
+        checking if the inner_keys exist in the dict else create them filling them with None
+        """
         check = {}
         flat_headers = self.flat_headers()
         for header in flat_headers:
@@ -62,6 +77,9 @@ class CleanPostFrame:
         return check
 
     def flat_dict(self, nested_dict):
+        """
+        flattening a dict
+        """
         out_dict = {}
         for key, val in nested_dict.items():
             if type(val) == dict:
@@ -72,6 +90,11 @@ class CleanPostFrame:
         return out_dict
 
     def perform_update_dict(self):
+        """
+        first grabs all the pd.Series converting to dicts
+        then flattens each dict
+        then checks if the key exists else created empty value
+        """
         data = []
         for d in self.series_to_dict():
             flat_dict = self.flat_dict(d)
@@ -80,25 +103,18 @@ class CleanPostFrame:
         return data
         
     def create_df(self):
+        """
+        creates df from the unique headers and the returned data 
+        """
         columns = self.flat_headers()
         data = self.perform_update_dict()
         new_df = pd.DataFrame.from_dict(data)
         return new_df
 
     def df_to_csv(self):
+        """
+        converts df to csv
+        """
         df = self.create_df()
         df.to_csv('Cleaned_data.csv')
 
-
-
-
-
-if os.path.exists('Post_Data.json'):  # if files exists
-    post_data_json = pd.read_json('Post_Data.json')
-    data_frame = pd.read_json((post_data_json['data'].to_json()), orient='index')
-
-t = CleanPostFrame(data_frame)
-# a = t.create_df()
-# b = t.series_to_dict()
-# c = t.perform_update_dict()
-d = t.df_to_csv()
